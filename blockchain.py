855e5ef0a3bc742045f66381052f709713eee4ee
quantoums <oumarbajobs@gmail.com>
2023-04-22 01:15:20 +0200

    Addition of proof_of_work and valid_block methods !

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime
 from hashlib import sha256
+import random
 
 
 
@@ -14,7 +15,7 @@ class Blockchain(object):
         #create genesis block
         print(" \n---------------LIGHT ON !--------------------\n")
 
-        self.new_block()
+        self.proof_of_work()
 
     def get_chain(self):
           print(self.chain)
@@ -47,27 +48,58 @@ class Blockchain(object):
                 # create new block as dictionary, each block contains index, time, the previous_hash
                 # the transactions to validate are in the pending_transactions list
                 # each block has to validate those transactions 
+                # we get rid of pending transactions for the moment 
 
                 block = {'index':len(self.chain),
                          'timestamp' : datetime.utcnow().isoformat(),
-                         'transactions' : self.pending_transactions,
-                         'previous_hash' : self.previous_hash
+                         'transactions' : [],
+                         'previous_hash' : self.previous_hash,
+                         'nonce ' : format(random.getrandbits(8),"x")
                          }
                 # hash the block
                 hash = self.hasher(block)
+
                 # add the  hash to the block
                 block['hash'] = hash
 
-                # add the block to the chain
-                self.chain.append(block)
+                # return block
 
-                # update previous hash value
-                self.previous_hash = hash
-
-                # empty pending_transactions since they've been hashed and put to the block
-                self.pending_transactions = []
-                # return the block
                 return block
+
+                
+    # We stipulate that a block is valid if its hash begins with '0000'
+    # the method is static, we can use it outside of the classes object to see if any subjected block is valid 
+    @staticmethod
+    def valid_block(block):
+          return block['hash'].startswith('0000')
+    
+    # since in the block we have the field 'once" wich is a 64-bits hex number randomly generated
+    # the work consists of finding a block with a hash starting with "0000"
+    # if the block is not valid, we create a new one via the once randomly generated
+    # 
+
+    def proof_of_work(self):
+            while True:
+                  new_block = self.new_block()
+                  if self.valid_block(new_block):
+                        break
+           # add the block to the chain
+            self.chain.append(new_block)
+
+            # update previous hash value
+            self.previous_hash = new_block['hash']
+            if(len(self.chain)==1):
+
+                  print(f"\n Block {new_block['index']} : Genesis \n hash : {new_block['hash']} \n Ther are {len(self.chain)} blocks ! \n")
+            
+            else :
+                  print(f"\n Block {new_block['index']}\n hash : {new_block['hash']} \n Ther are {len(self.chain)} blocks ! \n")
+            
+          
+          
+          
+          
+
 
 
 
