673eb87b1f25ceb3f5e207cecb5f7f3d03b24015
quantoums <oumarbajobs@gmail.com>
2023-04-22 19:53:58 +0200

    echo_server.py which establish a connection btw server and client using asyncio lib

---
+++ echo_server.py
@@ -0,0 +1,29 @@
+import asyncio
+
+async def handle_connection(reader,writer):
+
+    writer.write("Hello new user, type something ... \n".encode())
+
+    data = await reader.readuntil(b"\n")
+
+    writer.write('You sent: '.encode() + data)
+
+    # wait for data to be transmitted write put it in a buffer but if the data not used  by os the buffer will grow 
+    # till we have memory issue. Drain wait for the data to be used by the os 
+    await writer.drain()
+
+    # close the socket/connection and clean-up
+
+    writer.close()
+
+    # wait for the socket to be closed
+    await writer.wait_closed()
+
+async def main():
+
+    server = await asyncio.start_server(handle_connection,"0.0.0.0",8888)
+
+    async with server :
+        await server.serve_forever()
+        
+asyncio.run(main())
